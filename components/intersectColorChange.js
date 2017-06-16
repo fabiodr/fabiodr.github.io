55138f14c7036342687d8d48629e66387e886f80
Fabio Dias Rollo <fabiodr@gmail.com>
2017-06-16 18:00:16 -0300

    Core cursor fix, wait for canvas to load before registering event handlers

--- components/intersectColorChange.js
+++ components/intersectColorChange.js
@@ -5,15 +5,28 @@ AFRAME.registerComponent('intersect-color-change', {
     var initialColor = material.color;
     var self = this;
 
-    el.addEventListener('mousedown', function (evt) {
-      el.setAttribute('material', 'color', '#EF2D5E');
+    // el.addEventListener('mousedown', function (evt) {
+    //   el.setAttribute('material', 'color', '#EF2D5E');
+    // });
+
+    el.addEventListener('click', function () {
+      console.log('intersect-color-change click');
+      el.setAttribute('material', 'color', '#000000');
+      self.isMouseEnter = false;
     });
+
+    // el.addEventListener('raycaster-intersected', function () {
+    //   //console.log('intersect-color-change raycaster-intersected');
+    //   el.setAttribute('material', 'color', '#FFFFFF');
+    //   self.isMouseEnter = false;
+    // });
 
     el.addEventListener('mouseup', function (evt) {
       el.setAttribute('material', 'color', self.isMouseEnter ? '#24CAFF' : initialColor);
     });
 
     el.addEventListener('mouseenter', function () {
+      console.log('intersect-color-change mouseenter');
       el.setAttribute('material', 'color', '#24CAFF');
       self.isMouseEnter = true;
     });
